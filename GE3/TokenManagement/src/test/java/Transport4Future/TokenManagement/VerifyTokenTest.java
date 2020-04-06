3800b78dbb0838d6efc372851df88a14d4aa0d14
Ricardo Díaz Acosta <100405923@alumnos.uc3m.es>
2020-04-06 10:59:31 +0200

    no message

--- GE3/TokenManagement/src/test/java/Transport4Future/TokenManagement/VerifyTokenTest.java
+++ GE3/TokenManagement/src/test/java/Transport4Future/TokenManagement/VerifyTokenTest.java
@@ -21,6 +21,35 @@ public class VerifyTokenTest {
 	}
 	
 	
+	/* Test case: Token is not found
+	* Testing technique: Structural analysis - <Basic paths – Loop tests>
+	* Expected value: Error message indicating the token couldnt be processed
+	*/
+	public void CP_RF2_01() throws TokenManagementException
+	{
+		TokenManager tm = new TokenManager();
+		String t = tm.RequestToken(InputFile);
+		try {
+			boolean verified = tm.VerifyToken(t);
+		} catch (TokenManagementException e) {
+			// TODO Auto-generated catch block
+			Assertions.assertEquals("Error: input string does not contain a token that can be processed.", e.message);
+		}
+	}
 	
-	
+	/* Test case: Token is found, expiration date < current time
+	* Testing technique: Structural analysis - <Basic paths – Loop tests>
+	* Expected value: Error message indicating the token couldnt be processed
+	*/
+	public void CP_RF2_02() throws TokenManagementException
+	{
+		TokenManager tm = new TokenManager();
+		String t = tm.RequestToken(InputFile);
+		try {
+			boolean verified = tm.VerifyToken(t);
+		} catch (TokenManagementException e) {
+			// TODO Auto-generated catch block
+			Assertions.assertEquals("Error: input string does not contain a token that can be processed.", e.message);
+		}
+	}
 }
